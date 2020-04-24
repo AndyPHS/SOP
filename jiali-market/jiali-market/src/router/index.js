50332314582f697239b0b7d00e3f5cb175b98102
patterson <1479009707@qq.com>
2020-04-24 19:06:34 +0800

    去除URL中的#

--- jiali-market/jiali-market/src/router/index.js
+++ jiali-market/jiali-market/src/router/index.js
@@ -10,14 +10,14 @@ import About from '@/components/About'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    
-    {path: '/',name: 'Home',component: Home},
-    {path: '/CaiChan',name: 'CaiChan',component: CaiChan},
-    {path: '/ZiNv',name: 'ZiNv',component: ZiNv},
-    {path: '/JiCheng',name: 'JiCheng',component: JiCheng},
-    {path: '/Team',name: 'Team',component: Team},
-    {path: '/About',name: 'About',component: About},
-    {path: '*',redirect: '/'}
-  ]
+	mode: 'history',
+	routes: [
+		{path: '/',name: 'Home',component: Home},
+		{path: '/CaiChan',name: 'CaiChan',component: CaiChan},
+		{path: '/ZiNv',name: 'ZiNv',component: ZiNv},
+		{path: '/JiCheng',name: 'JiCheng',component: JiCheng},
+		{path: '/Team',name: 'Team',component: Team},
+		{path: '/About',name: 'About',component: About},
+		{path: '*',redirect: '/'}
+	]
 })
